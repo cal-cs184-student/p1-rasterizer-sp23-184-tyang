b93ddec2e7b505b977d84aa6d0ffd9e24f7999de
Cveinnt <wuws.vincent@gmail.com>
2023-02-08 20:18:08 -0800

    implemented sample_bilinear

--- src/texture.cpp
+++ src/texture.cpp
@@ -35,7 +35,6 @@ namespace CGL {
 
 
 
-
     // return magenta for invalid level
     return Color(1, 0, 1);
   }
@@ -44,8 +43,19 @@ namespace CGL {
     // TODO: Task 5: Fill this in.
     auto& mip = mipmap[level];
 
+    float tx = uv.x * (mip.width-1);
+    float ty = uv.y * (mip.height-1);
 
+    Color t1 = mip.get_texel(floor(tx), floor(ty));
+    Color t2 = mip.get_texel(floor(tx), ceil(ty));
+    Color t3 = mip.get_texel(ceil(tx), ceil(ty));
+    Color t4 = mip.get_texel(ceil(tx), floor(ty));
 
+    Color lerp1 = t1 + (tx - floor(tx)) * (t4 + (-1 * t1));
+    Color lerp2 = t2 + (tx - floor(tx)) * (t3 + (-1 * t2));
+    Color lerp3 = lerp1 + (ty - floor(ty)) * (lerp2 + (-1 * lerp1));
+
+    return lerp3;
 
     // return magenta for invalid level
     return Color(1, 0, 1);
